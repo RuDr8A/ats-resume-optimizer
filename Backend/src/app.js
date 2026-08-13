9a2f1b648453edda0869e6bbd1354344ef59f1a8
RuDr8A <rudrasingh240310@gmail.com>
2026-08-14 00:48:38 +0530

    Update CORS to accept dynamic FRONTEND_URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,9 +10,9 @@ const app = express() ;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin : 'http://localhost:5173',
-    credentials : true
-}))
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+}));
 
 /* using all the routes here */
 app.use("/api/auth", authRouter)
